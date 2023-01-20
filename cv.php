82a67a6ee997d80d135d8da8bf3d1a8aad264324
Almira05 <123169038+Almira05@users.noreply.github.com>
2023-01-20 20:34:14 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>cv almira</title>
+<body><style>
+   
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    h3 {
+        border: 3px solid blue;
+    }
+    table {
+        width:100%;
+        margin:10px;
+        align: center;
+    }
+    
+    </style>
+    <marquee behavior="alternate">
+    <h2 align=center> CURRICULUM VITAE </h2>
+</marquee>
+<table style="width:100%">
+    <img src="Profil.jpg" 
+    style="width:150px;" align="center">
+    <table>
+    <div style="background-color:blue;color:white;">
+    <h3 align=center>DATA PRIBADI</h3>
+<tr>
+        <th align=left>Nama</th>
+        <td>: Almira fitria</td>
+</tr>
+<tr>
+        <th align=left>Tempat, Tanggal lahir</th>
+        <td>: Bandung, 20 Oktober 2005</td>
+</tr>
+<tr>
+        <th align=left>Jenis Kelamin</th>
+        <td>: Perempuan</td>
+</tr>
+<tr>
+        <th align=left>Agama</th>
+        <td>: Islam</td>
+</tr>
+<tr>
+        <th align=left>Alamat</th>
+        <td>: Kp. Cijeungjing RT 04 RW 13 Desa Kertajaya Kec Padalarang Kab Bandung Barat</td>
+</tr>
+<tr>
+        <th align=left>No Telepon</th>
+        <td>: 08987870832</td>
+</tr>
+<tr>
+        <th align=left>Email</th>
+        <td>: almira.pjm.5@gmail.com</td>
+</tr>
+</table>
+<table>
+<div style="background-color:blue;color:white;">
+<h3 align=center>KEAHLIAN</h3>
+</div>
+<tr>
+   
+    <p align=center>MICROSOFT EXCEL</p>
+
+    <p align=center>MICROSOFT WORLD</p> 
+</tr>
+<tr>
+    <p align=center>HTML</p> 
+</tr>
+<tr>
+    <p align=center>CSS</p> 
+</tr>
+<tr>
+    <p align=center>PHP</p> 
+</tr>
+</table>
+<table>
+<div style="background-color:blue;color:white;">
+<h3 align=center>PENGALAMAN KERJA</h3>
+<tr>
+    <h4 align=center>TELAH MELAKSANAKAN KEGIATAN PKL DI PT ALFA POLIMER INDONESIA</h4>
+</tr>
+</table>
+<table style="width:100%"; align="center";>
+<div style="background-color:blue;color:white;">
+<h3 align=center>RIWAYAT PENDIDIKAN</h3>
+<tr>
+    <th>2010-2012</th>
+    <td>TK GEMILANG</td>
+</tr>
+<tr>
+    <th>2012-2017</th>
+    <td>SD NEGERI 3 KERTAJAYA</td>
+</tr>
+<tr>
+    <th>2017-2020</th>
+    <td>SMP KRIDA UTAMA</td>
+</tr>
+</table>
+</body>
+</html>
